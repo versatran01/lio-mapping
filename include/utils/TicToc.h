57d4af57bdf4c4b5d99e32998eddbf45d2544b6e
Chao Qu <versatran01@gmail.com>
2019-08-03 13:41:10 -0400

    update

--- include/utils/TicToc.h
+++ include/utils/TicToc.h
@@ -1,28 +1,28 @@
 /**
-* This file is part of LIO-mapping.
-* 
-* Copyright (C) 2019 Haoyang Ye <hy.ye at connect dot ust dot hk>,
-* Robotics and Multiperception Lab (RAM-LAB <https://ram-lab.com>),
-* The Hong Kong University of Science and Technology
-* 
-* For more information please see <https://ram-lab.com/file/hyye/lio-mapping>
-* or <https://sites.google.com/view/lio-mapping>.
-* If you use this code, please cite the respective publications as
-* listed on the above websites.
-* 
-* LIO-mapping is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* LIO-mapping is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with LIO-mapping.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of LIO-mapping.
+ *
+ * Copyright (C) 2019 Haoyang Ye <hy.ye at connect dot ust dot hk>,
+ * Robotics and Multiperception Lab (RAM-LAB <https://ram-lab.com>),
+ * The Hong Kong University of Science and Technology
+ *
+ * For more information please see <https://ram-lab.com/file/hyye/lio-mapping>
+ * or <https://sites.google.com/view/lio-mapping>.
+ * If you use this code, please cite the respective publications as
+ * listed on the above websites.
+ *
+ * LIO-mapping is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LIO-mapping is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LIO-mapping.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 //
 // Created by hyye on 3/14/18.
@@ -31,19 +31,15 @@
 #ifndef LIO_TICTOC_H
 #define LIO_TICTOC_H
 
-#include <cstdlib>
 #include <chrono>
+#include <cstdlib>
 #include <iostream>
 
 class TicToc {
  public:
-  TicToc() {
-    Tic();
-  }
+  TicToc() { Tic(); }
 
-  void Tic() {
-    start_ = std::chrono::system_clock::now();
-  }
+  void Tic() { start_ = std::chrono::system_clock::now(); }
 
   double Toc() {
     end_ = std::chrono::system_clock::now();
@@ -51,13 +47,11 @@ class TicToc {
     return elapsed_seconds_.count() * 1000;
   }
 
-  double GetLastStop() {
-    return elapsed_seconds_.count() * 1000;
-  }
+  double GetLastStop() { return elapsed_seconds_.count() * 1000; }
 
  private:
   std::chrono::time_point<std::chrono::system_clock> start_, end_;
   std::chrono::duration<double> elapsed_seconds_;
 };
 
-#endif //LIO_TICTOC_H
+#endif  // LIO_TICTOC_H
